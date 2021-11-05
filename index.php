2c651325b3ea2a5e82cff5c2f89c3abb585d197e
bennybriel <bennybriel@gmail.com>
2021-11-05 09:00:46 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+         //Check Returning URL
+		  $p="TRA21APP9JBC470";
+          $url = "https://gatewaylautech.bennybriel.com/index.php/api/getPaymentStatus/".$p;    
+$response = get_web_page($url);
+$resArr =array();
+$resArr = json_decode($response);
+echo $resArr->status;
+
+
+
+function get_web_page($url) {
+    $options = array(
+        CURLOPT_RETURNTRANSFER => true,   // return web page
+        CURLOPT_HEADER         => false,  // don't return headers
+        CURLOPT_FOLLOWLOCATION => true,   // follow redirects
+        CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
+        CURLOPT_ENCODING       => "",     // handle compressed
+        CURLOPT_USERAGENT      => "test", // name of client
+        CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
+        CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
+        CURLOPT_TIMEOUT        => 120,    // time-out on response
+    ); 
+
+    $ch = curl_init($url);
+    curl_setopt_array($ch, $options);
+
+    $content  = curl_exec($ch);
+
+    curl_close($ch);
+
+    return $content;
+}
+         
+?>
+
